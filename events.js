ed8e43d40b12eee898a7e203bd08a44fef7190ed
Sif Høg <cimmik@yahoo.dk>
2023-07-16 23:17:13 +0200

    Defined Neuron

--- events.js
+++ events.js
@@ -3,11 +3,20 @@ const DNA_SEQUENCE = document.getElementById("dna-sequence");
 const DNA_GENERATE = document.getElementById("generate-dna");
 
 ALLOW_DNA.addEventListener('change', (event) => {
-    if (this.checked) {
+    console.log("checked");
+    DNA_SEQUENCE.removeAttribute("disabled") 
+    if (ALLOW_DNA.checked) {
         DNA_SEQUENCE.removeAttribute("disabled");
         DNA_GENERATE.removeAttribute("disabled");
     } else {
         DNA_SEQUENCE.setAttribute("disabled", "");
         DNA_GENERATE.setAttribute("disabled", "");
     }
-  });
+});
+
+DNA_GENERATE.addEventListener("click", (event) => {
+    DNA_SEQUENCE.value = "";
+    for(let i = 0; i < 23; i++) {
+        DNA_SEQUENCE.value += Math.floor(Math.random()*10);
+    }
+});
--- sketch.js
+++ sketch.js
@@ -30,6 +30,52 @@ function setLightness(colour, lightness) {
     const A = alpha(colour);
     return color(`hsla(${H}, ${S}%, ${lightness}, ${A})`);
 }
+grid = [[]];
+
+class Neuron {
+    constructor(wb) {
+        this.weights = [];
+        this.bias = wb[wb.length];
+        for(let w = 0; w < wb.length-1; w++) {
+            thisweights.push(wb[w]);
+        }
+    }
+    activate(signalIn) {
+        if(signalIn.length != this.weights.length) {
+            console.error("Number of input signals to neuron must match number of weights");
+        }
+        let outputSignal = 0;
+        for(let w = 0; w < signalIn.length; w++) {
+            outputSignal += this.weights[w]*signalIn[w];
+        }
+        outputSignal += this.bias;
+        return outputSignal;
+    }
+}
+class NeuronLayer {
+    constructor(numberOfNeurons, numberOfWeights, wb) {
+        this.weightsAndBiases = wb;
+        this.neurons = [];
+        for(n = 0; n < numberOfNeurons; n++) {
+            this.neurons.push(new Neuron(this.weightsAndBiases.slice(0,numberOfWeights+1)));
+            this.weightsAndBiases = this.weightsAndBiases.slice(numberOfWeights+1);
+        }
+    }
+}
+
+class Grower {
+    constructor(x, y, dir, DNA) {
+        this.x = x;
+        this.y = y;
+        this.direction = dir;
+        this.dna = DNA;
+        this.neurons = [];
+        this.neurons.push(new NeuronLayer(3, 4, this.dna[0,15]));
+        this.dna = this.dna.slice(15);
+        this.neurons.push(new NeuronLayer(3, 3, this.dna));
+        this.dna = DNA;
+    }
+}
 
 function setup() {
     const DISPLAY = document.getElementById("display");
